00d80789320d263a2369b434a46aca8a8d4faf3e
aritzrxmxs <aritzramos11@gmail.com>
2025-06-28 14:15:17 +0200

    Se agrega el proyecto

---
+++ Variables/variables.py
@@ -0,0 +1,30 @@
+# Variables en Python
+
+#Declaración de variables en Python
+
+edad = 28
+altura = 1.65
+pais = 'España'
+
+# Acceder a las variables
+print('Valores Iniciales: ')
+print ('Edad:', edad)
+print('Altura:', altura)
+print('País:', pais)
+
+# Modificar el valor de la variable
+print('Valores modificados: ')
+edad = 29
+altura = 1.81
+
+print ('Edad:', edad)
+print('Altura:', altura)
+print('País:', pais)
+
+# En Python el tipo es dinámico
+edad = 'treinta'
+print('Edad:', edad)
+
+# Si queremos acceder a una variable no declarada, hay error.
+telefono = '664244538'
+print('Telefono:', telefono)
---
+++ proyectoInicial/presentacion.py
@@ -0,0 +1,4 @@
+# Ejercicio de Presentacion con Python
+print('Nombre: Aritz Ramos')
+print('Edad: 28')
+print('Pais: España')
