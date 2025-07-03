973b40cd637239cd2737c01cfff45354e1a54ddd
aritzrxmxs <alvictoria09@gmail.com>
2025-07-03 19:34:44 +0200

    Novedades 03/07

--- Operadores/operadores_asignacion.py
+++ Operadores/operadores_asignacion.py
@@ -6,4 +6,32 @@ print(f'Nuevo valor de numero: {numero}')
 
 cadena = 'Saludos desde Python'
 print(f'Valor de la cadena: {cadena}')
+# Asignación múltiple
+valor1, valor2, valor3 = 20, 'Aritz', False
 
+print(f'''
+Valor 1: {valor1}
+Valor 2: {valor2}
+Valor 3: {valor3}''')
+
+# Asignación encadenada
+
+contador1 = contador2 = 10
+
+print(f'''
+Valor contador 1: {contador1}
+Valor contador 2: {contador2}''')
+
+# Intercambio de valores en una variable, sin utilizar variables temporales
+x, y = 5, 10
+print(f'Valores iniciales x = {x}, y = {y}')
+# Aplicando el concepto de asignación múltiple, intercambiamos valores
+x, y = y, x
+
+print(f'Invertir los valores x = {x}, y = {y}')
+
+# Recibir múltiples valores de la entrada del usuario
+nombre, apellido = input('Ingresa tu nombre y apeliidos separados por coma: ').split(',')
+print(f'''
+Nombre: {nombre.strip()}
+Apellido: {apellido.strip()}''')
---
+++ Operadores/operadores_asignacion_compuestos.py
@@ -0,0 +1,23 @@
+print('*** Operadores de Asignación compuestos ***')
+
+a, b = 10, 15
+print(f'Valor inicial a: {a}, b: {b}')
+
+# Operador compuesto de suma +=
+a += b
+print(f'Operador a += b es: {a}')
+
+# Operador compuesto de resta -=
+a = 10
+a -= b
+print(f'Operador a -= b es: {a}')
+
+# Operador compuesto de multiplicación *=
+a = 10
+a *= b
+print(f'Operador a *= b es: {a}')
+
+# Operador compuesto de división /=
+a = 10
+a /= b
+print(f'Operador de a /= b es: {a:.2f}')
