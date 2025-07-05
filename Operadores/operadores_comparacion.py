4f241bbd8c39111ed9dcd7e1b83c54fed621ed21
aritzrxmxs <alvictoria09@gmail.com>
2025-07-05 14:21:58 +0200

    Novedades 05/07

---
+++ Operadores/operadores_comparacion.py
@@ -0,0 +1,21 @@
+# Operadores comparación
+
+a, b = 7, 5
+print(f'Valor inicial a {a}, b: {b}')
+# Operaor iguldad ==
+resultdo = (a == b)
+print(f'Resultado a == b es: {resultdo}')
+
+# Operador diferente !=
+resultdo = a != b
+print(f'Resultado a != b es: {resultdo}')
+
+# Operador mayor que >
+resultado = a > b
+print(f' Resultado a > b es: {resultado}')
+
+# Operador mayor o igual que >=
+resultado = a >= b
+print(f'Resultado a >= b es: {resultado}')
+
+# Operador menor que
